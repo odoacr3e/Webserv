92ade2888c792c8aedf37c6242b835da83f98c9b
Edoardo Raccanello <edraccan@c1r4p5.42firenze.it>
2025-12-08 12:49:06 +0100

    defined configuration file parsing

--- srcs/hpp/Conf.hpp
+++ srcs/hpp/Conf.hpp
@@ -3,9 +3,32 @@
 
 # include "../../includes/ether.hpp"
 
+
+// #events
+// #http
+// # server
+// #  location
+
+// #events minimo 0, massimo 1
+// #http 1 e uno solo
+// #server minimo 1, massimo INFINITI
+// #location minimo 0, massimo INFINITI
+
+//getline
+//find(something different from ISSPACE)
+// if npos
+//	IGNORE
+//1)	prendi prima cosa che non e ISSPACE fino a ISSPACE successiva
+//2)	push back in un vector
+//3)	togliere dalla linea letta
+//4.1)	quando trovi ';', analizzi la stringa, ripeti punto 1
+//4.2)	quando trovi '{', entri strato/dai errore, ripeti punto 1
+//4.3)	quando trovi '}', esci strato/dai errore, ripeti punto 1.
+
 class Conf
 {
 	private:
+
 	//canonic
 	public:
 		Conf();

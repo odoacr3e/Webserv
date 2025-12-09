c0245ff1f792cba30b1e1255d6308bb30fc1cf4b
edoRacca <edoraccanello@gmail.com>
2025-12-09 23:25:15 +0100

    contiued parsing setting up switch conditions for blocks, sorry guys <3

--- srcs/parsing/conf/parseMain.cpp
+++ srcs/parsing/conf/parseMain.cpp
@@ -2,14 +2,33 @@
 #include "../../../includes/ether.hpp"
 #include "../../hpp/Conf.hpp"
 
+int closeBlock(Conf &conf)
+{
+	if (conf.getHttp() && !conf.getServer() && !conf.getLocation() && !conf.getEvents())
+		conf.setHttp(false);
+	else if (conf.getHttp() && conf.getServer() && !conf.getLocation() && !conf.getEvents())
+		conf.setServer(false);
+	else if (conf.getHttp() && conf.getServer() && conf.getLocation() && !conf.getEvents())
+		conf.setLocation(false);
+	return (1);
+}
+
 int	newOpenBlock(Conf &conf, std::vector<std::string> &list, int *i)
 {
-	if (list.size() != 1)
-		throw Conf::ConfException("Error in format configuration in line " + *i);
-	if (list[0] == "http" && conf.getHttp() == false)
+	std::cout << "List size: " << list.size() << std::endl;
+	std::cout << "List val: " << list[0] << std::endl;
+	if (list.size() != 1 && list[0] != "location")
+		throw Conf::ConfException("Error in format configuration in line " + (*i));
+	if (list[0] == "http" && !conf.getHttp())
 		conf.setHttp(true);
+	//server dentro ad http non gia aperto e fuori da location o da events
+	else if (list[0] == "server" && conf.getHttp() && !conf.getServer() && !conf.getLocation() && !conf.getEvents())
+		conf.setServer(true);
+	else if (list[0] == "location" && conf.getServer() && conf.getHttp() && !conf.getLocation() && !conf.getEvents())
+		conf.setLocation(true);
 	else
-		throw Conf::ConfException("Impossible to open http block into another in line " + *i);
+		throw Conf::ConfException("Impossible to open http block into another in line " + (*i));
+	list.clear();
 	return (1);
 }
 
@@ -30,8 +49,8 @@ void	parseMain(Conf &conf, std::ifstream &fd)
 			line = removeWhitespaces(line);
 			if (line[0] == '{')
 				newOpenBlock(conf, list, &i);
-			// else if (line[0] == '}')
-				// newOpenBlock();
+			else if (line[0] == '}')
+				closeBlock(conf);
 			// else if (line[0] == ';')
 				// newOpenBlock();
 			else 

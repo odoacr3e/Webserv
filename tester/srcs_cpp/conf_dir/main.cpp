304a92699a3f2e17bbceb4c963c0d753ce82b678
alerusso <alessandro.russo.frc@gmail.com>
2026-03-04 23:12:13 +0100

    fixed compilation error in conf_tester

--- tester/srcs_cpp/conf_dir/main.cpp
+++ tester/srcs_cpp/conf_dir/main.cpp
@@ -4,11 +4,12 @@ int	main(int ac, char **av)
 {
 	std::string		output;
 	int				exit_code = 0;
+	char * const	new_argv[] = {(char *)PROG_NAME, av[1], NULL};
 
 	if (ac == 1)
 		return (1);
 	chdir(PROG_PATH);
-	exec_cgi((char*const[]){(char *)PROG_NAME, av[1], NULL}, output, true);
+	run_cmd(new_argv, output, true);
 	if (ac == 3 && av[2] == (std::string)"update")
 		exit_code = update_result(av[1], output);
 	else
